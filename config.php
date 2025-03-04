7ce58d2094751e9155928cd9d32231d84e027c0d
Lilian Carrière <130406171+Dimitri-Bliat@users.noreply.github.com>
2025-03-04 09:38:45 +0100

    Update config.php
    petit pb avec les merges pour le bdd

--- config.php
+++ config.php
@@ -2,13 +2,15 @@
 
 
 $host = 'localhost';
-$db = 'compte_utilisateur';
+$db_users = 'compte_utilisateur';
+$db_optiplant = 'optiplant';
 $user = 'root';
 $pass = '';
 $port = '3306';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;port=$port;dbname=$db_users;charset=$charset";
+$dsn_optiplant = "mysql:host=$host;port=$port;dbname=$db_optiplant;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -21,5 +23,10 @@ try {
      throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 
-
+try {
+     $pdo_optiplant = new PDO($dsn_optiplant, $user, $pass, $options);
+     $pdo_optiplant->exec("SET time_zone = '+01:00'");
+} catch (\PDOException $e) {
+     die('<div class="alert alert-danger text-center" role="alert">Erreur de connexion à la base de données : ' . htmlspecialchars($e->getMessage()) . '</div>');
+}
 ?>
