24c8b626c8d699729a754ae7a4faaced9bb5301d
Lilian Carrière <liliancarriere@gmail.com>
2025-03-05 15:28:40 +0100

    changement du modal de val

--- dashboard.php
+++ dashboard.php
@@ -103,9 +103,9 @@ $alerts = $stmt->fetchAll(PDO::FETCH_ASSOC);
         </div>
     </div>
     <!-- Modale (page volante) -->
-    <div id="modal" class="modal">
-        <div class="modal-content">
-            <span class="close-modal" id="close-modal">&times;</span>
+    <div id="modal" class="modal1">
+        <div class="modal1-content">
+            <span class="close-modal1" id="close-modal">&times;</span>
             <h2>Paramètres de couleurs</h2>
 
             <div class="grid">
