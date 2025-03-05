6402ac50e7a0d880db404e554d49897ce885618e
Guillaume Seimandi <guillaume.seimandi@hotmail.com>
2025-03-05 22:11:37 +0100

    Encore les accents dans le script meteo

--- dashboard.php
+++ dashboard.php
@@ -325,7 +325,7 @@ $alerts = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
-    <script src="js/météo_script.js"></script>
+    <script src="js/meteo_script.js"></script>
 </body>
 
 </html>
